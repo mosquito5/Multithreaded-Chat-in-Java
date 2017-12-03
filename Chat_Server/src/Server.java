413b0b99cc2eb87124a8b0b71dc46d65df037eec
mosquito <jakub.moskal3@gmail.com>
2017-12-03 19:34:31 +0100

    generated files on 2017-12-03 19:34:31

--- Chat_Server/src/Server.java
+++ Chat_Server/src/Server.java
@@ -1,44 +1,71 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
 
 public class Server {
     private int port;
-    private int threadNumber;
+    //private int threadNumber;
     private ServerSocket serverSocket;
-    //static Socket clientSocket = null;
-    public ArrayList<ClientThread> clientList;
     //do wczytania bajtu w metodzie menu
     private char menuChar;
     private boolean stopped;
     private Scanner scanner;
+    private String input;
     boolean SocketsConnected;
+    boolean isValid;
     public ServerThread serverThread;
     public SimpleDateFormat simpleDateFormat;
     public String msg = null;
 
 
-    //default settings
     public Server() {
         port = 4694;
+        isValid = true;
         //simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
-        threadNumber = 1;
+        //threadNumber = 1;
         serverSocket = null;
-        //do testow
-        //clientList = new ArrayList<ClientThread>();
+
         stopped = false;
         SocketsConnected = false;
         scanner = new Scanner(System.in);
     }
 
+    private void setUp() {
+        System.out.println("Chcesz skonfigurowac serwer? [t/n/a-atomatycznie]");
+
+        input = scanner.nextLine();
+
+        if(input.equals("n")){
+                System.out.println("Serwer zostanie uruchoamiony z domyślnymi wartosciami \nport: " + port);
+        }
+        else if (input.equals("t")) {
+            do {
+                System.out.print("Podaj port: ");
+                port = getPort();
+            }while (!isValid);
+
+            //System.out.println("Serwer zostanie uruchoamiony z wartosciami \nport: " + port);
+        }
+        else if(input.equals("a"))
+        /************************************************
+         * podczas tworzenia scoketu serwera,           *
+         * zostanie port zostanie alokowany atumatycznie*
+         ************************************************/
+            port = 0;
+        else {
+            System.out.println("[t/n/a-atomatycznie]");
+        }
+        System.out.println();
+    }
+
     public void start() {
         try {
             serverSocket = new ServerSocket(port);
-            serverThread = new ServerThread(serverSocket/*, THREADS, MAX_CLIENTS*/);
+            serverThread = new ServerThread(serverSocket);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -48,6 +75,7 @@ public class Server {
             while (!stopped){
             ShowMenu();
                 try {
+                    //do zmiany
                     menuChar = (char) System.in.read();
                     /***************
                      * czysci bufor*
@@ -79,10 +107,10 @@ public class Server {
         System.out.println("Menu:\nw-wiadomosc\nq-wyjscie\nl-lista podlaczonych klientow\nm-wyswietla ponownie menu");
     }
     private void sendMessage() {
-        String input;
         System.out.print("Wiadomość: ");
         serverThread.sendToClients("SERWER: " + getInput());
     }
+
     private String getInput() {
         /*************************
          * czysci bufor ze smieci*
@@ -92,6 +120,23 @@ public class Server {
          * wczytuje znak*
          ****************/
         return scanner.nextLine();
+    }
+
+    private int getPort() {
+        int value = -1;
+        try{
+            value = scanner.nextInt();
+            if(!isValid)
+                isValid = true;
+        } catch (InputMismatchException none) {
+            System.out.println("Port musi byc liczba");
+            isValid = false;
+        }
+        if(value < 1023 || value > 65535) {
+            System.out.println("Zbyt wysoki/niski port");
+            isValid = false;
+        }
+        return value;
     }
 
     /********************
@@ -110,7 +155,10 @@ public class Server {
 
     public static void main(String args[]) {
         Server server = new Server();
+        server.setUp();
         server.start();
+        System.out.println("Serwer uruchomiony pomyslnie\nPort: " + server.serverSocket.getLocalPort()
+                + "\nAdres: " + server.serverSocket.getInetAddress() + "\n");
         server.menu();
         server.close();
     }
